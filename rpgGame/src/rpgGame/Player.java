50981b9cfaace206a727482492d66d7889ab5b42
kimseungwoo449 <ehdanf449@naver.com>
2024-04-01 15:20:18 +0900

    add : 생성자, reciveExp,checkLevelUp

--- rpgGame/src/rpgGame/Player.java
+++ rpgGame/src/rpgGame/Player.java
@@ -1,5 +1,24 @@
 package rpgGame;
 
-public class Player {
+abstract public class Player extends Unit {
+	private int lv;
 
+	public Player(String name, int maxHp, int offensivePower, int exp) {
+		super(name, maxHp, offensivePower, exp);
+		this.lv = 1;
+	}
+
+	public void reciveExp(int exp) {
+		String info = String.format("[%s]가 %d의 경험치를 얻었습니다!.", super.getName(), exp);
+		System.out.println(info);
+		super.setExp(exp);
+	}
+
+	public void checkLevelUp() {
+		if (super.getExp() >= 100) {
+			System.out.println("레벨 업!");
+			this.lv += 1;
+			super.levelUp();
+		}
+	}
 }
