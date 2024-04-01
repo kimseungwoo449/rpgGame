3430bf99a6fbda94715c8002267e488ec0813c4c
kimseungwoo449 <ehdanf449@naver.com>
2024-04-01 15:42:48 +0900

    add : 생성자

--- rpgGame/src/rpgGame/UnitSlime.java
+++ rpgGame/src/rpgGame/UnitSlime.java
@@ -1,5 +1,13 @@
 package rpgGame;
 
-public class UnitSlime {
+public class UnitSlime extends Unit{
 
+	public UnitSlime() {
+		super("슬라임", 120, 15, 10);
+	}
+	
+	@Override
+	public int skill(Unit target) {
+		return 0;
+	}
 }
--- rpgGame/src/rpgGame/UnitWolf.java
+++ rpgGame/src/rpgGame/UnitWolf.java
@@ -1,5 +1,13 @@
 package rpgGame;
 
-public class UnitWolf {
+public class UnitWolf extends Unit{
 
+	public UnitWolf() {
+		super("늑대", 150, 20, 17);
+	}
+	
+	@Override
+	public int skill(Unit target) {
+		return 0;
+	}
 }
