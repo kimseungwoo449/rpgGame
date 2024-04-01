6e4b076abcc51c74b36c94876b9eadf29d721619
kimseungwoo449 <ehdanf449@naver.com>
2024-04-01 15:59:47 +0900

    add : inputNumber, inputString

--- rpgGame/src/rpgGame/GameManager.java
+++ rpgGame/src/rpgGame/GameManager.java
@@ -1,5 +1,24 @@
 package rpgGame;
 
-public class GameManager {
+import java.util.Scanner;
 
+public class GameManager {
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static String inputString(String message) {
+		System.out.print(message + " : ");
+		return sc.next();
+	}
+	
+	public static int inputNumber(String message) {
+		int number = -1;
+		try {
+			System.out.print(message + " : ");
+			String input = sc.next();
+			number = Integer.parseInt(input);
+		} catch (Exception e) {
+			System.err.println("숫자로 입력해 주세요.");
+		}
+		return number;
+	}
 }
