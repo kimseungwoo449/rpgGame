c501fe39f86e3b4f46ed3b9fb79d0c190778db00
kimseungwoo449 <ehdanf449@naver.com>
2024-04-01 15:38:12 +0900

    modify : skill

--- rpgGame/src/rpgGame/PlayerWizard.java
+++ rpgGame/src/rpgGame/PlayerWizard.java
@@ -1,6 +1,6 @@
 package rpgGame;
 
-public class PlayerWizard extends Player{
+public class PlayerWizard extends Player {
 
 	public PlayerWizard(String name, int maxHp, int offensivePower, int exp) {
 		super(name, maxHp, offensivePower, exp);
@@ -9,9 +9,11 @@ public class PlayerWizard extends Player{
 	@Override
 	public int skill(Unit target) {
 		System.out.println("[파이어 볼] 발동!");
-		String info = String.format("[%s]가 [%s]에게 %d의 대미지를 입혔습니다!", super.getName(), target.getName(), (int)super.getOffensivePower()*1.5);
+		int myOffensivePower = super.getOffensivePower();
+		int attack = myOffensivePower + (int) (myOffensivePower * 1.5);
+		String info = String.format("[%s]가 [%s]에게 %d의 대미지를 입혔습니다!", super.getName(), target.getName(), attack);
 		System.out.println(info);
-		return (int) (super.getOffensivePower()*1.5);
+		return attack;
 	}
 
 }
