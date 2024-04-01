12189248294ac8eb4552c0865dfac9b40999b60f
kimseungwoo449 <ehdanf449@naver.com>
2024-04-01 16:53:08 +0900

    add : 생성자, printMenu, runMenu, getInstance

--- rpgGame/src/rpgGame/StageLobby.java
+++ rpgGame/src/rpgGame/StageLobby.java
@@ -1,5 +1,32 @@
 package rpgGame;
 
-public class StageLobby {
-
+public class StageLobby extends Stage{
+	private final int BATTLE = 1;
+	private final int EXIT =2;
+	
+	private static StageLobby instance = new StageLobby();
+	
+	private StageLobby() {
+		super("LOBBY");
+	}
+	
+	public static StageLobby getInstance() {
+		return instance;
+	}
+	
+	@Override
+	public void printMenu() {
+		System.out.println("======["+super.getStageName()+"]=====");
+		System.out.println("[1] 전투 [2] 종료");
+		runMenu();
+	}
+	
+	private void runMenu() {
+		int choice = GameManager.inputNumber("Menu");
+		if(choice==BATTLE) {
+			
+		}
+		else if(choice==EXIT)
+			GameManager.isRun = false;
+	}
 }
