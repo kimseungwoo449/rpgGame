4a9ea80796dfd530143b7825c6ad0c97c8eaaff4
kimseungwoo449 <ehdanf449@naver.com>
2024-04-01 18:36:27 +0900

    modify : runMenu <- 배틀상황

--- rpgGame/src/rpgGame/StageLobby.java
+++ rpgGame/src/rpgGame/StageLobby.java
@@ -1,32 +1,31 @@
 package rpgGame;
 
-public class StageLobby extends Stage{
+public class StageLobby extends Stage {
 	private final int BATTLE = 1;
-	private final int EXIT =2;
-	
+	private final int EXIT = 2;
+
 	private static StageLobby instance = new StageLobby();
-	
+
 	private StageLobby() {
 		super("LOBBY");
 	}
-	
+
 	public static StageLobby getInstance() {
 		return instance;
 	}
 	
 	@Override
 	public void printMenu() {
-		System.out.println("======["+super.getStageName()+"]=====");
+		System.out.println("========[" + super.getStageName() + "]=======");
 		System.out.println("[1] 전투 [2] 종료");
 		runMenu();
 	}
-	
+
 	private void runMenu() {
 		int choice = GameManager.inputNumber("Menu");
-		if(choice==BATTLE) {
-			
-		}
-		else if(choice==EXIT)
+		if (choice == BATTLE) {
+			GameManager.battle = true;
+		} else if (choice == EXIT)
 			GameManager.isRun = false;
 	}
 }
