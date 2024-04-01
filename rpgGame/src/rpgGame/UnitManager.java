f57ae384531f9b659c655e44b8c5d1f126a94695
kimseungwoo449 <ehdanf449@naver.com>
2024-04-01 17:24:55 +0900

    add : UnitManager 생성자 add : 몬스터 랜덤 생성 add : getInstance

--- rpgGame/src/rpgGame/UnitManager.java
+++ rpgGame/src/rpgGame/UnitManager.java
@@ -1,5 +1,47 @@
 package rpgGame;
 
-public class UnitManager {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class UnitManager {
+	private Random ran = new Random();
+	private String path;
+	private String monsterName[];
+	
+	private static UnitManager instance = new UnitManager();
+	
+	private UnitManager() {
+		monsterName = new String[] { "UnitOrc", "UnitSlime", "UnitWolf" };
+	}
+
+	public ArrayList<Unit> createMonsters() {
+		ArrayList<Unit> monsters = new ArrayList<Unit>();
+		Class<?> params[] = new Class<?>[] { int.class, int.class, int.class };
+		int rNum = ran.nextInt(5);
+		for (int i = 0; i < rNum; i++) {
+			int rIndex = ran.nextInt(3);
+			path = "rpgGame." + monsterName[rIndex];
+
+			try {
+				Class<?> clazz = Class.forName(path);
+				int hp = ran.nextInt(100) + 120;
+				int power = ran.nextInt(10) + 15;
+				int exp = ran.nextInt(15) + 10;
+				Object obj = clazz.getDeclaredConstructor(params).newInstance(hp, power, exp);
+				Unit temp = (Unit)obj;
+				monsters.add(temp);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+
+		return monsters;
+	}
+	
+	public static UnitManager getInstance() {
+		return instance;
+	}
+	
 }
