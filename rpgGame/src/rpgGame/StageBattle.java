7df105924d944532c7a138254c40fd2b0f49c09a
kimseungwoo449 <ehdanf449@naver.com>
2024-04-01 18:35:53 +0900

    add : stageBattle 생성자 <- 플레이어와 몬스터 리스트를 받아 진행 add : printMenu <- 플레이어와 몬스터 현황 보여줌 add : checkNext <- 몬스터가 다죽거나 플레이어가 죽으면 배틀상황 종료 add : runMenu add : attack <- 이미 죽은 몬스터 때리지 못함, 전사캐릭터면 스킬 사용후 버프 가지고 있어 추가, 플레이어 공격 add : takeDamage 플레이어 몬스터들에게 대미지 입음

--- rpgGame/src/rpgGame/StageBattle.java
+++ rpgGame/src/rpgGame/StageBattle.java
@@ -1,5 +1,123 @@
 package rpgGame;
 
-public class StageBattle {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class StageBattle extends Stage {
+	private Random ran = new Random();
+
+	private final int ATTACK = 1;
+	private final int SKILL = 2;
+
+	private Player player;
+
+	private ArrayList<Unit> monsters;
+
+	public StageBattle(Player player, ArrayList<Unit> monsters) {
+		super("BATTLE");
+		this.player = player;
+		this.monsters = monsters;
+	}
+
+	@Override
+	public void printMenu() {
+		System.out.println("========[" + super.getStageName() + "]=======");
+		while (checkNext()) {
+			System.out.println("========[PLAYER]=======");
+			System.out.println(player);
+			System.out.println("========[MONSTER]=======");
+			for (Unit monster : monsters)
+				System.out.println(monster);
+			System.out.println("[1] 어택 [2] 스킬");
+			runMenu();
+		}
+		result();
+		GameManager.battle = false;
+	}
+
+	private boolean checkNext() {
+		int numberOfMonster = this.monsters.size();
+		int count = 0;
+		for (Unit monster : monsters) {
+			if (monster.isDead())
+				count++;
+		}
+
+		if (count != numberOfMonster && !player.isDead()) {
+			return true;
+		}
+		return false;
+	}
+
+	private void runMenu() {
+		int choice = GameManager.inputNumber("Menu");
+		if (choice == ATTACK)
+			attack();
+		else if (choice == SKILL)
+			skill();
+	}
+
+	private void attack() {
+		Unit monster = null;
+		while (true) {
+			int rMonster = ran.nextInt(monsters.size());
+			monster = monsters.get(rMonster);
+
+			if (!monster.isDead())
+				break;
+		}
+		int attack = player.getOffensivePower();
+		
+		if (player instanceof PlayerWarrior && ((PlayerWarrior) player).isSkilled()) {
+			attack*=3;
+			((PlayerWarrior) player).setIsSkilled();
+		}
+
+		monster.takeDamage(player, attack);
+		takeDamage();
+	}
+
+	private void takeDamage() {
+		for (Unit monster : monsters) {
+			if (!monster.isDead() && !player.isDead()) {
+				int attack = monster.getOffensivePower();
+				player.takeDamage(monster, attack);
+			}
+		}
+	}
+
+	private void skill() {
+		Unit monster = null;
+		while (true) {
+			int rMonster = ran.nextInt(monsters.size());
+			monster = monsters.get(rMonster);
+
+			if (!monster.isDead())
+				break;
+		}
+		if (player instanceof PlayerWizard) {
+			int attack = player.skill(monster);
+			monster.takeDamage(player, attack);
+			takeDamage();
+		} else if (player instanceof PlayerPaladin || player instanceof PlayerWarrior) {
+			player.skill(monster);
+			takeDamage();
+		}
+
+	}
+
+	private void result() {
+		if (player.isDead()) {
+			System.err.println("GAME OVER...");
+			GameManager.isRun = false;
+			return;
+		}
+
+		for (Unit monster : monsters) {
+			int exp = monster.getExp();
+			player.setExp(exp);
+			player.checkLevelUp();
+		}
+	}
 
 }
