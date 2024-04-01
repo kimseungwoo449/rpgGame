99eda5ac1388f14ce6073face57732bea6a1a19f
kimseungwoo449 <ehdanf449@naver.com>
2024-04-01 16:54:26 +0900

    add : 생성자, getStageName

--- rpgGame/src/rpgGame/Stage.java
+++ rpgGame/src/rpgGame/Stage.java
@@ -1,5 +1,15 @@
 package rpgGame;
 
 abstract public class Stage {
+	private String stageName;
+	
+	public Stage(String name) {
+		this.stageName = name;
+	}
+	
+	public String getStageName() {
+		return this.stageName;
+	}
+	
 	abstract public void printMenu();
 }
